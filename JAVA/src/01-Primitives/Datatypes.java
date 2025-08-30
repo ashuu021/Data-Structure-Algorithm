a4103fea0ea362214d3bb814500d0c9b886bae38
ashuu021 <ashuu023@gmail.com>
2025-08-30 22:03:13 +0530

    Implement basic Java data types

---
+++ JAVA/src/01-Primitives/Datatypes.java
@@ -0,0 +1,13 @@
+class Datatype{
+    public static void main(String[] args) {
+        // int roll_number = 23;
+        // String name = "Anshu";
+        // char alphabet = 's';
+        // boolean isLogin = true;
+        // float marks = 34.232f;
+        // double largeDecimal = 3434242.3232322;
+        // long largeInteger = 232334343231L;
+
+
+    }
+}
