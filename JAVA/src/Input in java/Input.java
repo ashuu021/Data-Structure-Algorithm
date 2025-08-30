52eb09e0fff4456907a8b5b301f15e44b2baa68f
ashuu021 <ashuu023@gmail.com>
2025-08-30 21:44:42 +0530

    Implement variables and input/output

---
+++ JAVA/src/Input in java/Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Input {
+
+public static void main(String[] args) {
+    System.out.println("Enter your name");
+Scanner input = new Scanner(System.in);
+System.out.println(input.next()); //Take the str(one word)
+System.out.println(input.nextLine()); //Take the str(set of words)
+
+System.out.println(input.nextInt());//Take the integer
+
+} 
+}
