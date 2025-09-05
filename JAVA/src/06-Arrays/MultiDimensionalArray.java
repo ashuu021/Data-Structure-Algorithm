b0cd02e05350449b3105f76035cd0210691d635e
ashuu021 <ashuu023@gmail.com>
2025-09-05 23:01:05 +0530

    Add Java program:MultiDimensionalArray

---
+++ JAVA/src/06-Arrays/MultiDimensionalArray.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        // int[][] arr=new int[3][];
+        int[][] twoD_arr={
+            {1,2,23},
+            {2,3},
+            {4,22,1,7}
+        };
+
+    }
+}
