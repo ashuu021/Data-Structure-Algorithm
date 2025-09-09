00bf94e4939e186b48a028857ed22455fff396e4
ashuu021 <ashuu023@gmail.com>
2025-09-09 14:46:19 +0530

    public class SearchIn2DArray { Add Java program:

---
+++ JAVA/src/07-Sorting Algorithms/Linear Search Algorithm/SearchIn2DArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int [][] arr={
+            {1,2,32},
+            {0,23,3,12},
+            {9,6,44,8,5}
+        };
+        int target=0;
+        int[] ans=search(arr, target);
+
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] search(int [][] arr,int target){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+              if ( arr[i][j]==target) {
+                return new int[]{i,j};
+              }
+               
+            }
+        }
+return new int[]{-1,-1};
+    }
+}
