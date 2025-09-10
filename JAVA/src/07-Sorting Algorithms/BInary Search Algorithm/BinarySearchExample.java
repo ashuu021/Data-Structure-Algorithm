dd6f32cea33c1dc8b03a0bfe3ad08830d875ee70
ashuu021 <ashuu023@gmail.com>
2025-09-10 23:16:20 +0530

    Add Java program:BinarySearchExample

--- JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/BinarySearchExample.java
+++ JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/BinarySearchExample.java
@@ -1,8 +1,8 @@
 public class BinarySearchExample {
 
     public static void main(String[] args) {
-        int [] nums={0,1,2,3,4};
-        int target=3;
+        int [] nums={0,1,5,7,11,22,134};
+        int target=22;
         System.out.println(search(nums, target));
     }
     static int search(int [] arr,int target){
@@ -10,10 +10,10 @@ public class BinarySearchExample {
     int end = arr.length-1;
     while (start<=end) {
         int mid = start+(end-start)/2;
-        if (mid==target) {
+        if (arr[mid]==target) {
             return mid;
         }
-        else if (mid<target) {
+        else if (arr[mid]<target) {
             start=mid+1;
         }
         else{
