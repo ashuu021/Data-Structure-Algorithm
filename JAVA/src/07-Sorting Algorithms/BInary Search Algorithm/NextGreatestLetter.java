1afa3cf3768dfa93e67811d06a6337ed506990ef
ashuu021 <ashuu023@gmail.com>
2025-09-12 15:27:05 +0530

    Add Java program: NextGreatestLetter

--- JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/NextGreatestLetter.java
+++ JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/NextGreatestLetter.java
@@ -1,5 +1,25 @@
 public class NextGreatestLetter {
     public static void main(String[] args) {
-        
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int st = 0;
+        int end = letters.length - 1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+
+        // wrap-around
+        return letters[st % letters.length];
     }
 }
