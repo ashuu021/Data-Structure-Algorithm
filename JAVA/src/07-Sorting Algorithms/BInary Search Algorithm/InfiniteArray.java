e4bbc0fe573741825cab659a7589f304337a8079
ashuu021 <ashuu023@gmail.com>
2025-09-12 19:16:02 +0530

    Update Java program:InfiniteArray

--- JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/InfiniteArray.java
+++ JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/InfiniteArray.java
@@ -8,20 +8,14 @@ public class InfiniteArray {
         System.out.println(ans(arr, target));
     }
     static int ans(int[] arr, int target) {
-        // first find the range
-        // first start with a box of size 2
-        int start = 0;
-        int end = 1;
-
-        // condition for the target to lie in the range
-        while (target > arr[end]) {
-            int temp = end + 1; // this is my new start
-            // double the box value
-            // end = previous end + sizeofbox*2
-            end = end + (end - start + 1) * 2;
-            start = temp;
-        }
-        return binarySearch(arr, target, start, end);
+       int start=0;
+       int end=1;
+       while (target>arr[end]) {
+            int newStart=end+1;
+            end=end+(end-start+1)*2;
+            start=newStart;        
+       }
+       return binarySearch(arr, target, start, end);
 
     }
     static int binarySearch(int[] arr, int target, int start, int end) {
