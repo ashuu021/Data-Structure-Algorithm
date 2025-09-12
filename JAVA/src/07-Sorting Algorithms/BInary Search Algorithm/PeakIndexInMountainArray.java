12c8e92aabdfb5b086df8ca0245048eb04757a36
ashuu021 <ashuu023@gmail.com>
2025-09-12 19:48:57 +0530

    Add Java program:PeakIndexInMountainArray

--- JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/PeakIndexInMountainArray.java
+++ JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/PeakIndexInMountainArray.java
@@ -1,3 +1,20 @@
 public class PeakIndexInMountainArray {
-    
+    public static void main(String[] args) {
+        int []arr = {0,10,5,2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    static int peakIndexInMountainArray(int[] arr){
+        int st=0;
+        int end =arr.length-1;
+        while (st<end) {
+            int mid =st+(end-st)/2;
+            if (arr[mid]>arr[mid+1]) {
+                end=mid;
+            }else{
+                st=mid+1;
+            }
+            
+        }
+        return st;
+    }
 }
