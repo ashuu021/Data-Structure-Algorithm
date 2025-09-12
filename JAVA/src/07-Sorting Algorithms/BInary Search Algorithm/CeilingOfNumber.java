9485a614a3616aa54cfbb027f6a7aa5765491f12
ashuu021 <ashuu023@gmail.com>
2025-09-12 14:52:48 +0530

    Add Java program:CeilingOfNumber

--- JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/CeilingOfNumber.java
+++ JAVA/src/07-Sorting Algorithms/BInary Search Algorithm/CeilingOfNumber.java
@@ -1,3 +1,27 @@
 public class CeilingOfNumber {
-    
+     public static void main(String[] args) {
+        int [] nums={0,1,5,6,7,11,18,24,32,134};
+        int target=22;
+        System.out.println(search(nums, target));
+    }
+    static int search(int [] arr,int target){
+    int start =0;
+    int end = arr.length-1;
+    if (target>arr[arr.length]) {
+        return -1;
+    }
+    while (start<=end) {
+        int mid = start+(end-start)/2;
+        if (arr[mid]==target) {
+            return mid;
+        }
+        else if (arr[mid]<target) {
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+return start;
+    }
 }
