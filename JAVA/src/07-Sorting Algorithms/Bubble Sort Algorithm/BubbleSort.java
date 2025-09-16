adec90e11c68c0bf86d74be7600a0ba53f941e15
ashuu021 <ashuu023@gmail.com>
2025-09-16 22:09:26 +0530

    a

---
+++ JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int [] arr={1,4,52,6,72,9};
+search(arr);;
+
+
+    }
+    static void search(int[] arr){
+ for (int index = 0; index < arr.length; index++) {
+    for (int i = 0; i < arr.length-1-index; i++) {
+        if (arr[i]<arr) {
+            
+        }
+    }
+    
+ }
+    }
+}
