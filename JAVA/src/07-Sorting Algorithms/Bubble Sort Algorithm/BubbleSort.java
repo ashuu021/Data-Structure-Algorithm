418a08b7acafdb823c0abc039cee37135b474a6d
ashuu021 <ashuu023@gmail.com>
2025-09-17 23:05:55 +0530

    .

--- JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/BubbleSort.java
+++ JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/BubbleSort.java
@@ -1,19 +1,38 @@
 public class BubbleSort {
-
     public static void main(String[] args) {
-        int [] arr={1,4,52,6,72,9};
-search(arr);;
+        int[] arr = {5, 3, 8, 4, 2}; // example array
 
+        bubbleSort(arr);
 
-    }
-    static void search(int[] arr){
- for (int index = 0; index < arr.length; index++) {
-    for (int i = 0; i < arr.length-1-index; i++) {
-        if (arr[i]<arr) {
-            
+        // print sorted array
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
-    
- }
+
+    // Bubble Sort function
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        // run n-1 passes
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            // compare adjacent elements
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // if no swapping happened, array is already sorted
+            if (!swapped) break;
+        }
     }
 }
