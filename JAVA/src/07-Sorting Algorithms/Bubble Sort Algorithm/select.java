e0efe33395b1132196678c47a9ea5f82aedef996
ashuu021 <ashuu023@gmail.com>
2025-09-19 23:28:03 +0530

    a

---
+++ JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/select.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class select {
+    public static void main(String[] args) {
+        int[] arr={7,4,1,5,2};
+    sort(arr);
+    // System.out.println(arr);\
+    System.out.println(Arrays.toString(arr));
+   }  
+    static int arr
+}
