b7ec5328c45c612acb3e13aa00e17a82427b7843
ashuu021 <ashuu023@gmail.com>
2025-09-21 22:53:46 +0530

    Remove main method and clean up selection sort implementation in select.java

--- JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/select.java
+++ JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/select.java
@@ -1,11 +1,22 @@
-import java.util.Arrays;
-
 public class select {
-    public static void main(String[] args) {
-        int[] arr={7,4,1,5,2};
-    sort(arr);
-    // System.out.println(arr);\
-    System.out.println(Arrays.toString(arr));
-   }  
-\}}
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
 }
