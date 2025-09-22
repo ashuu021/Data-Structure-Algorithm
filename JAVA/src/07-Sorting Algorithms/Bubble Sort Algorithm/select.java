11916d34f5afcb1be9af66ced260d91ad4663c17
ashuu021 <ashuu023@gmail.com>
2025-09-22 21:45:34 +0530

    changes

--- JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/select.java
+++ JAVA/src/07-Sorting Algorithms/Bubble Sort Algorithm/select.java
@@ -1,22 +1,22 @@
-public class select {
+// public class select {
 
-    public static void selectionSort(int[] arr) {
-        int n = arr.length;
+//     public static void selectionSort(int[] arr) {
+//         int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
+//         for (int i = 0; i < n - 1; i++) {
+//             int minIndex = i;
 
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
+//             for (int j = i + 1; j < n; j++) {
+//                 if (arr[j] < arr[minIndex]) {
+//                     minIndex = j;
+//                 }
+//             }
 
-            if (minIndex != i) {
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
-            }
-        }
-    }
-}
+//             if (minIndex != i) {
+//                 int temp = arr[i];
+//                 arr[i] = arr[minIndex];
+//                 arr[minIndex] = temp;
+//             }
+//         }
+//     }
+// }
