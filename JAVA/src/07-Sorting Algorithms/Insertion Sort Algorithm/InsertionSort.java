3ca3c53edf4af8ae78c407d75fc8afc3a8c61c19
ashuu021 <ashuu023@gmail.com>
2025-09-24 19:11:29 +0530

    Add Java program:InsertionSort

---
+++ JAVA/src/07-Sorting Algorithms/Insertion Sort Algorithm/InsertionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class InsertionSort {
+public static void main(String[] args) {
+    int[] arr={0,9,4,7,1};
+insertionSort(arr);
+System.out.println(Arrays.toString(arr));
+}
+static void insertionSort( int[]arr){
+for (int i = 0; i < arr.length-1; i++) {
+    for (int j = i+1; j >0; j--) {
+        if (arr[j]<arr[j-1]) {
+            swap(arr, j, j-1);
+        }else{
+            break;
+        }
+    }
+    
+}
+}
+ static void swap(int[] arr, int i, int j) {
+     int temp = arr[i];
+     arr[i] = arr[j];
+     arr[j] = temp;
+ }   
+}
